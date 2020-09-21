4e0d3817a2405e409ea2374269e54a93e963ce14
MaugouMio <maugoumine@gmail.com>
2020-09-21 14:20:08 +0800

    劈空斬

--- build.bat
+++ build.bat
@@ -1,2 +1,5 @@
 mcdp make MapleCraft data pack
+rd /S /Q "C:\Users\user\AppData\Roaming\.minecraft\saves\MapleCraft test\datapacks\MapleCraft data pack"
+xcopy "C:\Users\user\Desktop\MapleCraft\MapleCraft data pack\build\MapleCraft data pack" "C:\Users\user\AppData\Roaming\.minecraft\saves\MapleCraft test\datapacks\MapleCraft data pack" /E /Y /I /Q
+rd /S /Q "C:\Users\user\Desktop\MapleCraft\MapleCraft data pack\build\MapleCraft data pack"
 pause
