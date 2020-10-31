24de0def4b2fc5eb69babed35224ac930af9c72c
MaugouMio <maugoumine@gmail.com>
2020-10-31 22:58:02 +0800

    新增基本loot table
    新增楓幣掉落機制與材料道具模型
    generate_item_models.py可以把丟進etc資料夾中的道具圖片自動建立模型

---
+++ generate_item_models.py
@@ -0,0 +1,51 @@
+import os, json
+from PIL import Image
+
+etc_texture_folder = r"MapleCraft resource pack\assets\minecraft\textures\item\etc"
+etc_model_folder = r"MapleCraft resource pack\assets\minecraft\models\item\etc"
+
+try:
+	for folder in os.listdir(etc_texture_folder):
+		texture_parent_path = os.path.join(etc_texture_folder, folder)
+		if os.path.isdir(texture_parent_path):
+			model_parent_path = os.path.join(etc_model_folder, folder)
+			if not os.path.isdir(model_parent_path):
+				os.mkdir(model_parent_path)
+				
+			for file in os.listdir(texture_parent_path):
+				img_size = Image.open(os.path.join(texture_parent_path, file)).size
+				file = file[:file.find('.')]
+				with open(os.path.join(model_parent_path, file + ".json"), "w") as f:
+					model = {
+						"parent": "minecraft:item/generated",
+						"textures": {
+							"layer0": f"minecraft:item/etc/{folder}/{file}"
+						},
+						"display": {
+							"ground": {
+								"rotation": [ 0, 0, 0 ],
+								"translation": [ 0, 2, 0 ],
+								"scale": [ 0.5 * img_size[0] / 32, 0.5 * img_size[1] / 32, 0.5 ]
+							},
+							"gui": {
+								"rotation": [ 0, 0, 0 ],
+								"translation": [ 0, 0, 0 ],
+								"scale": [ 1 * img_size[0] / 32, 1 * img_size[1] / 32, 1 ]
+							},
+							"thirdperson_righthand": {
+								"rotation": [ 0, 0, 0 ],
+								"translation": [ 0, 3, 1 ],
+								"scale": [ 0.55 * img_size[0] / 32, 0.55 * img_size[1] / 32, 0.55 ]
+							},
+							"firstperson_righthand": {
+								"rotation": [ 0, -90, 25 ],
+								"translation": [ 1.13, 3.2, 1.13],
+								"scale": [ 0.68 * img_size[0] / 32, 0.68 * img_size[1] / 32, 0.68 ]
+							}
+						}
+					}
+					f.write(json.dumps(model))
+					
+except Exception as e:
+	print(e)
+	os.system("pause")
