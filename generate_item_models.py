c0ba9a2b7410a6b30ab9334634b1cf4dfdfe0870
MaugouMio <maugoumine@gmail.com>
2020-12-30 14:44:29 +0800

    新增手套道具素材

--- generate_item_models.py
+++ generate_item_models.py
@@ -1,7 +1,7 @@
 import os, json
 from PIL import Image
 
-for item_type in ["equip/overall"]:
+for item_type in ["equip/gloves"]:
 	texture_folder = "MapleCraft resource pack/assets/minecraft/textures/item/" + item_type
 	model_folder = "MapleCraft resource pack/assets/minecraft/models/item/" + item_type
 
