d4394a8ca62c3ce85630be4e0f256b42907454a4
MaugouMio <maugoumine@gmail.com>
2021-01-02 13:16:53 +0800

    新增臉飾裝備

--- set_equip_icon.py
+++ set_equip_icon.py
@@ -3,8 +3,8 @@ from PIL import Image, ImageDraw
 
 font = []
 back_font = []
-equip_type = "eye"
-font_index = int("0xD200", 16)
+equip_type = "face"
+font_index = int("0xD400", 16)
 
 if not os.path.isdir(f"MapleCraft resource pack/assets/ui/textures/font/window/equip/icon/{equip_type}"):
 	os.mkdir(f"MapleCraft resource pack/assets/ui/textures/font/window/equip/icon/{equip_type}")
