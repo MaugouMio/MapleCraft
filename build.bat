5a7fbff7b7a1e77fc73397d6e96b42b168ac0ba4
MaugouMio <maugoumine@gmail.com>
2021-02-18 21:24:01 +0800

    更改建置檔案路徑

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 mcdp make MapleCraft data pack
-rd /S /Q "C:\Users\user\AppData\Roaming\.minecraft\saves\MapleCraft test\datapacks\MapleCraft data pack"
-xcopy "C:\Users\user\Desktop\MapleCraft\MapleCraft data pack\build\MapleCraft data pack" "C:\Users\user\AppData\Roaming\.minecraft\saves\MapleCraft test\datapacks\MapleCraft data pack" /E /Y /I /Q
-rd /S /Q "C:\Users\user\Desktop\MapleCraft\MapleCraft data pack\build\MapleCraft data pack"
+rd /S /Q "C:\Users\MaugouMio\AppData\Roaming\.minecraft\saves\MapleCraft test\datapacks\MapleCraft data pack"
+xcopy "C:\Users\MaugouMio\Desktop\MapleCraft\MapleCraft data pack\build\MapleCraft data pack" "C:\Users\MaugouMio\AppData\Roaming\.minecraft\saves\MapleCraft test\datapacks\MapleCraft data pack" /E /Y /I /Q
+rd /S /Q "C:\Users\MaugouMio\Desktop\MapleCraft\MapleCraft data pack\build\MapleCraft data pack"
 pause
