9afe1ccabcf5727c33f0d29f6180385935435fd2
MaugouMio <maugoumine@gmail.com>
2023-03-15 00:13:27 +0800

    改用 filter 的方式關閉預設進度

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 mcdp make MapleCraft data pack
 rd /S /Q "C:\Users\MaugouMio\AppData\Roaming\.minecraft\saves\MapleCraft test\datapacks\MapleCraft data pack"
 xcopy "C:\Users\MaugouMio\Desktop\MapleCraft\MapleCraft data pack\build\MapleCraft data pack" "C:\Users\MaugouMio\AppData\Roaming\.minecraft\saves\MapleCraft test\datapacks\MapleCraft data pack" /E /Y /I /Q
+copy /Y "C:\Users\MaugouMio\Desktop\MapleCraft\pack.mcmeta" "C:\Users\MaugouMio\AppData\Roaming\.minecraft\saves\MapleCraft test\datapacks\MapleCraft data pack\pack.mcmeta"
 rd /S /Q "C:\Users\MaugouMio\Desktop\MapleCraft\MapleCraft data pack\build\MapleCraft data pack"
 pause
