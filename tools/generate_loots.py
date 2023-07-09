04e12b6f9796f04b2b3bae2cac5f6d1e59308b92
MaugouMio <maugoumine@gmail.com>
2023-07-09 23:23:44 +0800

    自動生成掉落表完整功能

--- tools/generate_loots.py
+++ tools/generate_loots.py
@@ -22,7 +22,6 @@ POOL_TEMPLATE = {
 }
 ENTRY_TEMPLATE = {
 	"type": "minecraft:item",
-	"weight": 1,
 	"name": "minecraft:warped_fungus_on_a_stick",
 	"functions": [
 		{
@@ -31,16 +30,18 @@ ENTRY_TEMPLATE = {
 		}
 	]
 }
+CHANCE_TEMPLATE = {
+	"condition": "minecraft:random_chance_with_looting",
+	"chance": 0,
+	"looting_multiplier": 1.0
+}
+COUNT_TEMPLATE = {
+	"function": "minecraft:set_count",
+	"count": 1
+}
 MESO_TEMPLATE = {
 	"type": "minecraft:item",
 	"name": "minecraft:diamond",
-	"conditions": [
-		{
-			"condition": "minecraft:random_chance_with_looting",
-			"chance": 0,
-			"looting_multiplier": 0.65
-		}
-	],
 	"functions": [
 		{
 			"function": "minecraft:set_attributes",
@@ -78,33 +79,108 @@ def LoadItem(dataFrame):
 		nbt = dataFrame["nbt標籤"][i]
 		if nbt == "":
 			continue
-		itemDatas[dataFrame["索引"][i]] = nbt
+		id = dataFrame["索引"][i]
+		itemDatas[id] = nbt
 
-LoadItem(sh[PAGE_WEAPON].get_as_df(include_tailing_empty=True))
-LoadItem(sh[PAGE_EQUIP1].get_as_df(include_tailing_empty=True))
-LoadItem(sh[PAGE_EQUIP2].get_as_df(include_tailing_empty=True))
-LoadItem(sh[PAGE_EQUIP3].get_as_df(include_tailing_empty=True))
-LoadItem(sh[PAGE_PROJECTILE].get_as_df(include_tailing_empty=True))
-LoadItem(sh[PAGE_POTION].get_as_df(include_tailing_empty=True))
-LoadItem(sh[PAGE_SCROLL].get_as_df(include_tailing_empty=True))
-LoadItem(sh[PAGE_CHAIR].get_as_df(include_tailing_empty=True))
-LoadItem(sh[PAGE_ETC].get_as_df(include_tailing_empty=True))
+LoadItem(sh[PAGE_WEAPON].get_as_df())
+LoadItem(sh[PAGE_EQUIP1].get_as_df())
+LoadItem(sh[PAGE_EQUIP2].get_as_df())
+LoadItem(sh[PAGE_EQUIP3].get_as_df())
+LoadItem(sh[PAGE_PROJECTILE].get_as_df())
+LoadItem(sh[PAGE_POTION].get_as_df())
+LoadItem(sh[PAGE_SCROLL].get_as_df())
+LoadItem(sh[PAGE_CHAIR].get_as_df())
+LoadItem(sh[PAGE_ETC].get_as_df())
 
 def GetItemID(index):
 	if index > 33000:
 		return "minecraft:stick"
 	return "minecraft:warped_fungus_on_a_stick"
+	
+def GenerateEntry(id, weight = 1, min_num = 1, max_num = 1):
+	if id == 0:  # 楓幣
+		entry = deepcopy(MESO_TEMPLATE)
+		amount = entry["functions"][0]["modifiers"][0]["amount"]
+		amount["min"] = min_num * 0.001
+		amount["max"] = max_num * 0.001
+	else:
+		entry = deepcopy(ENTRY_TEMPLATE)
+		entry["name"] = GetItemID(pool_id)
+		entry["functions"][0]["tag"] = itemDatas[pool_id]
+		if min_num > 1 or max_num > 1:
+			count_obj = deepcopy(COUNT_TEMPLATE)
+			if min_num == max_num:
+				count_obj["count"] = int(min_num)
+			else:
+				count_obj["count"] = { "min": int(min_num), "max": int(max_num) }
+			entry["functions"].append(count_obj)
+			
+	if weight > 1:
+		entry["weight"] = int(weight)
+		
+	return entry
+
+def SetChance(entry, chance):
+	chance_obj = deepcopy(CHANCE_TEMPLATE)
+	chance_obj["looting_multiplier"] = chance
+	entry["conditions"] = [chance_obj]
 
 # 建立掉落池表
-loot_pool_df = sh[PAGE_LOOT_POOL].get_as_df(include_tailing_empty=True)
+poolDatas = dict()
+loot_pool_df = sh[PAGE_LOOT_POOL].get_as_df()
+for i in range(loot_pool_df.shape[0]):
+	pool_id = loot_pool_df["索引"][i]
+	if pool_id == "":
+		continue
+	
+	entries = []
+	for j in range(60):
+		id_col = j * 4 + 2
+		id = loot_pool_df.iloc[i, id_col]
+		if id == "":
+			break
+		min_num = loot_pool_df.iloc[i, id_col + 1]
+		max_num = loot_pool_df.iloc[i, id_col + 2]
+		weight = loot_pool_df.iloc[i, id_col + 3]
+		entries.append(GenerateEntry(id, weight, min_num, max_num))
+		
+	poolDatas[pool_id] = entries
 
 # 輸出掉落表
-loot_df = sh[PAGE_LOOT].get_as_df(include_tailing_empty=True)
+loot_df = sh[PAGE_LOOT].get_as_df()
 for i in range(loot_df.shape[0]):
 	path = "../MapleCraft data pack/data/" + loot_df["路徑"][i].replace(":", "/loot_tables/") + ".json"
-	os.makedirs(os.path.dirname(path))
+	dir_name = os.path.dirname(path)
+	if not os.path.isdir(dir_name):
+		os.makedirs(dir_name)
 	
 	data = deepcopy(ROOT_TEMPLATE)
-	# do something
+	for j in range(60):
+		pool_col = j * 3 + 1
+		pool_id = loot_df.iloc[i, pool_col]
+		if pool_id == "":
+			break
+		chance = loot_df.iloc[i, pool_col + 2]
+		
+		pool = deepcopy(POOL_TEMPLATE)
+		pool["rolls"] = int(loot_df.iloc[i, pool_col + 1])
+		entries = pool["entries"]
+		if pool_id > 40000:  # 真的是掉落池表的內容
+			if chance < 1:
+				pool["entries"] = deepcopy(poolDatas[pool_id])
+				for entry in pool["entries"]:
+					SetChance(entry, chance)
+			else:
+				pool["entries"] = poolDatas[pool_id]
+		else:  # 單一道具編號
+			entry = GenerateEntry(pool_id)
+			if chance < 1:
+				SetChance(entry, chance)
+			pool["entries"] = [GenerateEntry(pool_id, chance)]
+		
+		data["pools"].append(pool)
+	
 	with open(path, "w") as f:
 		f.write(json.dumps(data))
+
+os.system("pause")
